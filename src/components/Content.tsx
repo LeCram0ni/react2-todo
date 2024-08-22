695b0cacb192b500e1c4dffb83cd2d2d0ddaaeab
marcel <15247414+LeCram0ni@users.noreply.github.com>
2024-08-22 18:59:52 +0200

    text+button

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,14 +1,34 @@
-import List from "./List"
+import { SetStateAction, useState } from "react";
+import List from "./List";
 
 export default function Content() {
+
+    const [inputValue, setInputValue] = useState('');
+
+    const handleInput = (event: { target: { value: SetStateAction<string>; }; }) => {
+        setInputValue(event.target.value);
+    }
+
+
+
+
+
+
+
+
+
+
     return (
         <div id="full">
             <div id="content">
                 <h1>
                     Your personal Todo List
                 </h1>
-                <input className="todo-textfield" type="text"></input>
-                <h2>Things that needs to be done</h2>
+                <div className="todo-text-button">
+                    <input className="todo-textfield" type="text" value={inputValue} onChange={handleInput} />
+                    <button className="todo-button">Add</button>
+                </div>
+                <h2>Things that need to be done</h2>
                 <List />
             </div>
         </div>
